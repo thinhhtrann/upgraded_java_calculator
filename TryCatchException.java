9c929f708def02827cf9cec4d668cf87ace9a009
Thinh Tran <timothy031102@gmail.com>
2025-05-28 19:08:19 -0400

    Initial commit

---
+++ TryCatchException.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+import java.lang.ArithmeticException;
+
+public class TryCatchException {
+	public static void main (String[] agrs){
+		
+	}
+}
